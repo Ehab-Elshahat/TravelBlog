e3ee647efcb07794680387c2cda50b95e987553e
Abo Anas <61202200+Ehab-Elshahat@users.noreply.github.com>
2020-06-21 13:46:52 +0300

    add nice hover on navbarlinks & change main color

--- js/code.js
+++ js/code.js
@@ -1,14 +1,14 @@
-// Add class active on navbar links 
+// Add class active on navbar links
 let navLinks = document.querySelectorAll(".navbar .links li a");
 
-navLinks.forEach(link => {
+navLinks.forEach((link) => {
   link.addEventListener("click", (e) => {
-    navLinks.forEach(lin => {
-      lin.classList.remove("active")
-    })
-    e.target.classList.add("active")
+    navLinks.forEach((lin) => {
+      lin.classList.remove("active");
+    });
+    e.target.classList.add("active");
   });
-})
+});
 
 // likes on swim
 let hearts = document.querySelectorAll(".activities .like .heart");
@@ -17,38 +17,30 @@ likeCounter(hearts);
 
 // function like counter
 function likeCounter(element) {
+  element.forEach((el) => {
+    el.addEventListener("click", function (e) {
+      e.target.parentElement.parentElement.querySelector(".count").innerHTML++;
+    });
+  });
+}
 
-  element.forEach(el => {
-
-    el.addEventListener('click',function(e) {
-
-        e.target.parentElement.parentElement.querySelector(".count").innerHTML++;
-        
-    })
-  })
-};
-
-// navbar menu toggler 
+// navbar menu toggler
 let togglerBtn = document.querySelector(".navbar .toggle-btn");
 let linksContainer = document.querySelector(".navbar .links-container");
 linksContainer.onclick = (e) => {
-  e.stopPropagation()
-}
+  e.stopPropagation();
+};
 
 togglerBtn.onclick = (e) => {
+  e.stopPropagation();
+  linksContainer.classList.toggle("open");
+};
 
-  e.stopPropagation()
-  linksContainer.classList.toggle("open")
-}
-
-document.addEventListener('click', (e) => {
-
-  if( e.target !== togglerBtn && e.target !== linksContainer) {
-    
-    linksContainer.classList.remove("open")
+document.addEventListener("click", (e) => {
+  if (e.target !== togglerBtn && e.target !== linksContainer) {
+    linksContainer.classList.remove("open");
   }
-
 });
 
-
-
+// Animation section
+AOS.init();
